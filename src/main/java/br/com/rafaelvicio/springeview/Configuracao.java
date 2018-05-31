a6e2b554dc2192166f72527c47b1306bb00427e9
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-31 14:48:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/rafaelvicio/springeview/Configuracao.java
@@ -0,0 +1,23 @@
+package br.com.rafaelvicio.springeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Configuracao {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String ola() {
+		return "Olá. Está tudo funcionando";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+}
